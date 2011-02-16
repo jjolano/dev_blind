bc37e02076a9e5939601265dcaefe72d68f25e79
John <john_olano43@hotmail.com>
2011-02-15 20:50:06 -0500

    version 1.1

--- source/main.c
+++ source/main.c
@@ -1,45 +1,37 @@
-#include <stdio.h>
-#include <malloc.h>
-#include <string.h>
-#include <assert.h>
-#include <unistd.h>
+// dev_blind - a (very simple) dev_flash write access mount enabler
+// by @jjolano
 
 #include <io/pad.h>
 #include <io/msg.h>
 
-#include <sysmodule/sysmodule.h>
-
 #include <sysutil/events.h>
+
+#include <lv2/process.h>
 
 #include <psl1ght/lv2.h>
 #include <psl1ght/lv2/filesystem.h>
 
 #include "rsxutil.h"
 
-int currentBuffer = 0;
-msgButton dlg_action = 0;
+const char* MOUNT_POINT = "/dev_blind"; // the directory where writable dev_flash should be mounted to
 
-void flipscreen()
-{
-	flip(currentBuffer); // Flip buffer onto screen
-	waitFlip(); // Wait for the last flip to finish, so we can draw to the old buffer
-	currentBuffer = !currentBuffer;
-}
+int currentBuffer = 0;
+msgButton dlg_action;
 
 void handledialog(msgButton button, void *userdata)
 {
 	dlg_action = button;
 }
 
-int direxists(const char* path)
+void handleevent(u64 status, u64 param, void * userdata)
 {
-	Lv2FsFile fd;
-	int ret = lv2FsOpenDir(path, &fd);
-	lv2FsCloseDir(fd);
-	return ret;
+	if(status == EVENT_REQUEST_EXITAPP)
+	{
+		sysProcessExit(0);
+	}
 }
 
-int showmessage(msgType type, const char* message)
+void showmessage(msgType type, const char* message)
 {
 	msgDialogOpen(type, message, handledialog, 0, NULL);
 	
@@ -47,12 +39,13 @@ int showmessage(msgType type, const char* message)
 	while(!dlg_action)
 	{
 		sysCheckCallback();
-		flipscreen();
+		
+		flip(currentBuffer);
+		waitFlip();
+		currentBuffer = !currentBuffer;
 	}
 	
 	msgDialogClose();
-	
-	return dlg_action;
 }
 
 int main(int argc, const char* argv[])
@@ -60,29 +53,37 @@ int main(int argc, const char* argv[])
 	msgType mdialogok	= MSGDIALOG_NORMAL | MSGDIALOG_BUTTON_TYPE_OK;
 	msgType mdialogyesno	= MSGDIALOG_NORMAL | MSGDIALOG_BUTTON_TYPE_YESNO;
 	
+	sysRegisterCallback(EVENT_SLOT0, handleevent, NULL);
+	
 	init_screen();
 	ioPadInit(7);
 	
 	waitFlip();
 	
-	showmessage(mdialogok, "dev_blind mounter created by @jjolano");
+	showmessage(mdialogyesno, "dev_blind v1.1 by jjolano (Twitter: @jjolano)\n\nThis program allows you to write to the flash memory of your console.\nDO NOT USE THIS PROGRAM IF YOU DON'T KNOW EXACTLY WHAT YOU ARE DOING!\n\nCapiche?");
 	
-	int is_mounted = direxists("/dev_blind");
-	int button = showmessage(mdialogyesno, (is_mounted == 0) ? "/dev_blind is already mounted - unmount?" : "Mount writable dev_flash to /dev_blind?");
-	
-	if(button == MSGDIALOG_BUTTON_OK)
+	if(dlg_action == MSGDIALOG_BUTTON_YES)
 	{
-		if(is_mounted == 0)
+		// see if dev_blind is mounted already
+		Lv2FsStat entry;
+		int is_mounted = lv2FsStat(MOUNT_POINT, &entry);
+		
+		showmessage(mdialogyesno, (is_mounted == 0) ? "Do you want to unmount dev_blind?" : "Do you want to mount dev_blind?");
+		
+		if(dlg_action == MSGDIALOG_BUTTON_YES)
 		{
-			// unmount /dev_blind
-			int ret = Lv2Syscall1(838, (u64)"/dev_blind");
-			showmessage(mdialogok, (ret == 0) ? "Successfully unmounted /dev_blind." : "Failed to unmount /dev_blind.");
-		}
-		else
-		{
-			// mount /dev_flash to /dev_blind
-			int ret = Lv2Syscall8(837, (u64)"CELL_FS_IOS:BUILTIN_FLSH1", (u64)"CELL_FS_FAT", (u64)"/dev_blind", 0, 0, 0, 0, 0);
-			showmessage(mdialogok, (ret == 0) ? "Successfully mounted writable dev_flash to /dev_blind.\nBe careful out there!" : "Failed to mount writable dev_flash to /dev_blind.");
+			if(is_mounted == 0)
+			{
+				// unmount dev_blind
+				Lv2Syscall1(838, (u64)MOUNT_POINT);
+				showmessage(mdialogok, "Successfully unmounted dev_blind.");
+			}
+			else
+			{
+				// mount dev_flash to dev_blind
+				Lv2Syscall8(837, (u64)"CELL_FS_IOS:BUILTIN_FLSH1", (u64)"CELL_FS_FAT", (u64)MOUNT_POINT, 0, 0, 0, 0, 0);
+				showmessage(mdialogok, "Successfully mounted dev_blind.");
+			}
 		}
 	}
 	
